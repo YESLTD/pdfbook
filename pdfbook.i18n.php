507889699ce059f0679b2934e927c8114066f65f
Thomas Kock <thomas.kock@gmx.de>
2012-02-25 00:17:21 +0100

    pdfbook-1.0.0-RC1

---
+++ pdfbook.i18n.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Internationalisation file for the ProtectSection extension
+ *
+ * @addtogroup Extensions
+ */
+ 
+$messages = array();
+ 
+/* English */
+$messages['en'] = array(
+    'pdfbook_ExportPDF' => 'Export to PDF'
+);
+	 
+/* Czech */
+$messages['cs'] = array(
+    'pdfbook_ExportPDF' => 'PDF export'
+);
+
+/* Slovak */
+$messages['sk'] = array(
+    'pdfbook_ExportPDF' => 'PDF export'
+);
+
+/* German */
+$messages['de'] = array(
+    'pdfbook_ExportPDF' => 'PDF erzeugen'
+);
+
+	
